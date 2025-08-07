df39efc206bcebe9388afeadfef722744076e15c
mayx2 <mmasc@discente.ifpe.edu.br>
2025-08-07 14:57:18 -0300

    adicionei a logo do aplicativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,13 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
+    
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
 }
--- app/src/main/java/com/mayra/to_do_list_mobile/listadetarefas/Tarefa.kt
+++ app/src/main/java/com/mayra/to_do_list_mobile/listadetarefas/Tarefa.kt
@@ -1,3 +1,7 @@
 package com.mayra.to_do_list_mobile.listadetarefas
 
 data class Tarefa(val id: Int, val nome: String, var isConcluida: Boolean = false)
+
+// Data Class é uma classe do próprio Android usada para armazenar dados, então ela está armazenando
+// os valores do meu banco em tarefa que são id, nome e isConcluida
+// Ele gera automáticamente funcionalidades para ajudar no código
